b9cd980df043670d4d8ce34c319836c5b9fafbb4
David Karlsson <david.karlsson1@learnet.se>
2019-10-25 13:49:20 +0200

    Add project files.

---
+++ Compare3Nums/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Compare3Nums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int bignumber = 7;
+
+            int number = 5;
+
+            int smallnumber = 0;
+
+            string bigstring = bignumber.ToString();
+            string Nstring = number.ToString();
+            string Smallstring = smallnumber.ToString();
+
+
+            if (bignumber > number && bignumber > smallnumber)
+            {
+             
+
+                           Console.WriteLine(bigstring);
+                           Console.WriteLine(" is the biggest number");
+
+            }
+
+
+            if (number > bignumber && number > smallnumber)
+            {
+
+
+                Console.WriteLine(Nstring);
+                Console.WriteLine(" is the biggest number");
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
